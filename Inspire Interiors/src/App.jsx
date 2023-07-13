218c5a09821ebef803d20193788cfe14580bb8aa
Shinthujen-I <shinthujeni@gmail.com>
2023-07-13 19:42:53 +0530

    Folder Structure and small components

---
+++ Inspire Interiors/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useState } from 'react'
+import Home from './pages/visitor/home';
+import './App.css'
+import Header from './components/header';
+
+
+
+ const App = () => {
+
+  return (
+
+      <Home />
+    
+  )
+}
+
+export default App
