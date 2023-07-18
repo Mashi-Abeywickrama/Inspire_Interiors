78aea887db97524d44336ee1f35bd5ffa5c7443e
Shinthujen-I <shinthujeni@gmail.com>
2023-07-18 22:04:53 +0530

    Sidebar and header Fixes

--- Inspire Interiors/src/components/navigationbar.jsx
+++ Inspire Interiors/src/components/navigationbar.jsx
@@ -7,25 +7,30 @@ import * as Icon from 'react-bootstrap-icons';
 
 import '../styles/navbar.css';
 
+import Logo from './../assets/img/logo.svg';
+import navbarImg from './../assets/img/visitor/navbarImg.png';
+
 const Navigationbar = () => {
     return (
+        <>
         <Navbar className='nav' expand="lg">
             <React.Fragment className="nav-div">
-                <Navbar.Brand  href="/"> <img src={require('./../assets/img/logo.svg')} alt="inspire interiors"/></Navbar.Brand>
-                <Navbar.Toggle  aria-controls="navbar-element"/>
-                <Navbar.Collapse id="navbar-element">
+                <Navbar.Brand  href="/"> <img src={Logo} alt="inspire interiors"/></Navbar.Brand>
+             
+               
                     <Nav className="ms-auto">
                         <Nav.Link className='form-group mx-4 main-div d-flex flex-row'>
-                          <input className='form-control-lg rounded-3 px-3' type='text' id='search' name='search' placeholder='Search'></input>
+                          
                           <div className='mx-4 my-2'><Icon.BellFill size={20} color='white'/></div>
-                          <img className='img-fluid bg-white rounded-2' src={require("../assets/img/navbarImg.png")} />
-                          <Nav.Link className='text-white mx-4 my-2'>Harish AR</Nav.Link>
+                          <img className='img-fluid bg-white rounded-2' src={navbarImg} />
+                          <Nav.Link className='text-white mx-4 my-2 d-none d-sm-block d-md-block d-lg-block'>Harish AR</Nav.Link>
                         </Nav.Link>
                     </Nav>
-                </Navbar.Collapse>
+            
 
             </React.Fragment>
         </Navbar>
+        </>
     );
 };
 
---
+++ Inspire Interiors/src/routes/vendorRoute.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.css';
+import Home from '../pages/visitor/home';
+import Contact from '../pages/visitor/contact';
+import Services from '../pages/visitor/services';
+import Error from '../pages/visitor/error';
+import MyOrder from '../pages/Customer/orders/my_orders';
+import Login from '../pages/visitor/login';
+import ViewOrder from '../pages/vendor/viewOrder';
+
+function VenderRoute() {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path='/vendor' element={<ViewOrder />}></Route>
+
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default VenderRoute
