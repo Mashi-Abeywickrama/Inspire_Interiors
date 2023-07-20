86bc7821575b57d601c9df5062c54fef44f5933c
Ahamed Rujhan <rujhanbheem123@gmail.com>
2023-07-20 15:35:35 +0530

    About Us...

--- Inspire Interiors/src/components/header.jsx
+++ Inspire Interiors/src/components/header.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
-import * as Icon from 'react-bootstrap-icons';
+import { useState } from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import * as Icon from "react-bootstrap-icons";
+import { LinkContainer } from "react-router-bootstrap";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/header.css';
-import Logo from './../assets/img/logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/header.css";
+import Logo from "./../assets/img/logo.svg";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  //array of items
+  const headerItems = [
+    "Home",
+    "About Us",
+    "Services",
+    "Our Projects",
+    "Our Team",
+    "Contact Us",
+  ];
+
+  const linkItems = ["/", "about", "services", "projects", "team", "contact"];
+
+  //hook
+  const [active, setActive] = useState(0);
   return (
     <Navbar className="nav" expand="lg">
       <div className="nav-div">
@@ -16,29 +32,96 @@ const Header = () => {
 
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link className="text-white m-2 jost-text" href="/">
-              Home
+          <Nav
+            className="ms-auto"
+            variant="underline"
+            defaultActiveKey={active}
+          >
+            <Nav.Link
+              className="text-white m-2 jost-text"
+              eventKey="0"
+              onClick={() => {
+                setActive(0);
+              }}
+            >
+              <NavLink to="/">Home</NavLink>
             </Nav.Link>
-            <Nav.Link className="text-white m-2 jost-text" href="/about">
+            <Nav.Link
+              className="text-white m-2 jost-text"
+              href="/about"
+              eventKey="1"
+              onClick={() => {
+                setActive(1);
+              }}
+            >
               About Us
             </Nav.Link>
-            <Nav.Link className="text-white m-2 jost-text" href="/services">
-              Services
+
+            <Nav.Link
+              className="text-white m-2 jost-text"
+              eventKey="2"
+              onClick={() => {
+                setActive(2);
+              }}
+            >
+              <NavLink to="/services">Services </NavLink>
             </Nav.Link>
-            <Nav.Link className="text-white m-2 jost-text" href="/projects">
+
+            <Nav.Link
+              className="text-white m-2 jost-text"
+              href="/projects"
+              eventKey="3"
+              onClick={() => {
+                setActive(3);
+              }}
+            >
               Our Projects
             </Nav.Link>
-            <Nav.Link className="text-white m-2 jost-text" href="/team">
+            <Nav.Link
+              className="text-white m-2 jost-text"
+              href="/team"
+              eventKey="4"
+              onClick={() => {
+                setActive(4);
+              }}
+            >
               Our Team
             </Nav.Link>
-            <Nav.Link className="text-white m-2 jost-text" href="/contact">
+            <Nav.Link
+              className="text-white m-2 jost-text"
+              eventKey="5"
+              onClick={() => {
+                setActive(5);
+              }}
+            >
               Contact Us
             </Nav.Link>
-            <Nav.Link className=" m-2 text-center jost-text login-btn" href="/login">
+            <Nav.Link
+              className=" m-2 text-center jost-text login-btn"
+              href="/login"
+              eventKey="-1"
+            >
               Login
             </Nav.Link>
           </Nav>
+          {/* <Nav variant="underline" defaultActiveKey="0" className="ms-auto">
+            {headerItems.map((item, index) => (
+              <Nav.Link
+                className="text-white m-2 jost-text nav-link text-light"
+                eventKey={index}
+                href={linkItems[index]}
+              >
+                {item}
+              </Nav.Link>
+            ))}
+            <Nav.Link
+              className=" m-2 text-center jost-text login-btn"
+              href="/login"
+              eventKey="-1"
+            >
+              Login
+            </Nav.Link>
+          </Nav> */}
         </Navbar.Collapse>
       </div>
     </Navbar>
--- Inspire Interiors/src/routes/visitorRoute.jsx
+++ Inspire Interiors/src/routes/visitorRoute.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.css';
-import Home from '../pages/visitor/home';
-import Contact from '../pages/visitor/contact';
-import Services from '../pages/visitor/services';
-import Error from '../pages/visitor/error';
-import MyOrder from '../pages/Customer/orders/my_orders';
-import Login from '../pages/visitor/login';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import Home from "../pages/visitor/home";
+import Contact from "../pages/visitor/contact";
+import Services from "../pages/visitor/services";
+import Error from "../pages/visitor/error";
+import MyOrder from "../pages/Customer/orders/my_orders";
+import Login from "../pages/visitor/login";
+import AboutUs from "../pages/visitor/AboutUs";
 
 function VisitorRoute() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/contact' element={<Contact />}></Route>
-            <Route path='/services' element={<Services />}></Route>
-            <Route path="/404" element={<Error />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/dev" element={<MyOrder />} />
-          	{/* <Route path="*" element={<Navigate to="/404" />} /> */}
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/services" element={<Services />}></Route>
+        <Route path="/404" element={<Error />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dev" element={<MyOrder />} />
+        <Route path="/about" element={<AboutUs />}></Route>
+        {/* <Route path="*" element={<Navigate to="/404" />} /> */}
 
-            {/* <Route path='/Blog' element={<Blog />}></Route> */}
-            {/* <Route path='/About' element={<About />}></Route> */}
-        </Routes>
+        {/* <Route path='/Blog' element={<Blog />}></Route> */}
+        {/* <Route path='/About' element={<About />}></Route> */}
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default VisitorRoute
+export default VisitorRoute;
