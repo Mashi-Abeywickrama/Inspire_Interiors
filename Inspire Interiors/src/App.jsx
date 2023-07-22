0f8dbb94e3c3e1e74d5ca17f3e9f9b8d43710c4e
Shinthujen-I <shinthujeni@gmail.com>
2023-07-22 19:50:19 +0530

    Small css fixes

--- Inspire Interiors/src/App.jsx
+++ Inspire Interiors/src/App.jsx
@@ -1,27 +1,28 @@
 
-import React from "react";
-import { useState } from "react";
+import React from 'react';
+import { useState } from 'react'
 
-import Home from "./pages/visitor/home";
-import Services from "./pages/visitor/services";
-import Contact from "./pages/visitor/contact";
-import ViewOrder from "./pages/vendor/viewOrder";
-import "./App.css";
-import Header from "./components/header";
-import MyOrder from "./pages/Customer/orders/my_orders";
-import VisitorRoute from "./routes/visitorRoute";
-import VenderRoute from "./routes/vendorRoute";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
-const App = () => {
+import './App.css';
+import MyOrder from './pages/Customer/orders/my_orders';
+import VisitorRoute from './routes/visitorRoute';
+import VenderRoute from './routes/vendorRoute';
+
+
+ const App = () => {
 
   return (
 
     <>
-      <VenderRoute />
-      <VisitorRoute />
+    <VenderRoute/>
+    <VisitorRoute/>
     </>
-  );
-};
+  
 
+  )
+}
 
 export default App;
