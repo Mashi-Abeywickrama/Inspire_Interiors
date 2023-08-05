a06042a7ec84e84b353a2f91b6608a276eeff9dd
Mashi <91023322+Mashi-Abeywickrama@users.noreply.github.com>
2023-08-05 13:52:41 +0530

    Customer designs update
    Designs and browse designs page

---
+++ Inspire Interiors/src/pages/Customer/Designs/browsedesigns.jsx
@@ -0,0 +1,221 @@
+import React from "react";
+import * as Icon from 'react-bootstrap-icons';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Pagination, Breadcrumb } from "react-bootstrap";
+import SearchPage from "../../../components/customer/filterNsearch";
+import './../../../styles/customer/designs.css';
+
+
+const CustomBullet = () => (
+    <span style={{ color: "orange", fontSize: "1.5em", marginRight: "0.5em" }}>
+        •
+    </span>
+);
+
+const data = [
+    {
+        image: "https://www.maticad.com/wp-content/uploads/2022/07/Colours_tips_maticad.jpg",
+        title: "Anxiety webinar, from etiology to treatment",
+        stars: 4.5,
+        votes: 50,
+        sold: 144,
+        price: 24,
+        column2: "Row 1, Column 2",
+    }, {
+        image: "https://cdn2.hubspot.net/hubfs/5208252/Corner%20RS%20110H%20Design%20Orly%20Dekter,%20Footage%20by%20Oded%20Smadar%206.jpg",
+        title: "Anxiety webinar, from etiology to treatment",
+        stars: 3.5,
+        votes: 50,
+        sold: 144,
+        price: 24,
+        column2: "Row 1, Column 2",
+    }, {
+        image: "https://cdn2.hubspot.net/hubfs/5208252/Corner%20RS%20110H%20Design%20Orly%20Dekter,%20Footage%20by%20Oded%20Smadar%206.jpg",
+        title: "Anxiety webinar, from etiology to treatment",
+        stars: 4.5,
+        votes: 50,
+        sold: 144,
+        price: 24,
+        column2: "Row 1, Column 2",
+    }, {
+        image: "https://www.maticad.com/wp-content/uploads/2022/07/Colours_tips_maticad.jpg",
+        title: "Anxiety webinar, from etiology to treatment",
+        stars: 4.5,
+        votes: 50,
+        sold: 144,
+        price: 24,
+        column2: "Row 1, Column 2",
+    }, {
+        image: "https://www.maticad.com/wp-content/uploads/2022/07/Colours_tips_maticad.jpg",
+        title: "Anxiety webinar, from etiology to treatment",
+        stars: 4.5,
+        votes: 50,
+        sold: 144,
+        price: 24,
+        column2: "Row 1, Column 2",
+    }, {
+        image: "https://www.maticad.com/wp-content/uploads/2022/07/Colours_tips_maticad.jpg",
+        title: "Anxiety webinar, from etiology to treatment",
+        stars: 4.5,
+        votes: 50,
+        sold: 144,
+        price: 24,
+        column2: "Row 1, Column 2",
+    },
+
+];
+
+
+const BrowseDesigns = () => {
+
+    const generateStars = (rate) => {
+        const fullStars = Math.floor(rate);
+        const halfStar = rate - fullStars >= 0.5;
+
+        const stars = [];
+        for (let i = 1; i <= 5; i++) {
+            if (i <= fullStars) {
+                stars.push(<Icon.StarFill key={i} color='#f39c12' />);
+            } else if (i === fullStars + 1 && halfStar) {
+                stars.push(<Icon.StarHalf key={i} color='#f39c12' />);
+            } else {
+                stars.push(<Icon.Star key={i} color='#f39c12' />);
+            }
+        }
+
+        return stars;
+    };
+
+
+    return (
+        <>
+            <div className="background d-flex flex-column justify-content-between w-100 bg-light rounded Cabin-text">
+                {/* Topic, search and filter */}
+                {/* Breadcrumb */}
+                <div className='bg-light top-bar py-3'>
+                    <div className='container'>
+                        <div className='row d-flex align-items-center'>
+                            <div className='col-md-4 col-sm-12 col-12 fs-4'>
+                                <Breadcrumb className="fw-bold">
+                                    <Breadcrumb.Item style={{ color: '#17183B !important' }}>
+                                        Browse Designs
+                                    </Breadcrumb.Item>
+                                    <Breadcrumb.Item active>Top Paid</Breadcrumb.Item>
+                                </Breadcrumb>
+                            </div>
+                            <div className='col-md-4 col-sm-6 col-6 text-center'>
+                            </div>
+                            <div className='col-md-4 col-sm-6 col-6 text-end'>
+                                <SearchPage />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Content in the Middle */}
+                <div className="py-1">
+                    <div className='container'>
+                        <div className='row flex-md-row flex-column'>
+                            {data.map((item, index) => (
+                                <div key={index} className="col-md-6 col-sm-6 col-12">
+                                    <div className="mb-3 bg-white p-3 rounded">
+
+                                        <div className="d-flex gap-2 flex-md-row flex-column" style={{ color: "#0A033C" }}>
+                                            <div className="w-25 align-self-center">
+                                                <img src={item.image} alt="Your Image" className="img-fluid rounded" />
+                                            </div>
+
+                                            <div className="w-75">
+
+                                                <div className="row">
+                                                    <div className="col-md-6 col-sm-6 col-6">
+                                                        <div className=" px-2 ">
+                                                            Row 1, Column 1
+                                                        </div>
+                                                    </div>
+
+                                                    <div className="col-md-6 col-sm-6 col-6">
+                                                        <div className="d-flex justify-content-end align-items-center">
+                                                            <li style={{ color: "#0B7077", listStyleType: "none" }}>
+                                                                <CustomBullet />
+                                                                Bedroom
+                                                            </li>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div className="row">
+                                                    <div className="col-12">
+                                                        <div className="fw-bold px-2">
+                                                            <p>{item.title}</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div className="row fw-semibold" style={{ color: '#696984' }}>
+                                                    <div className="col-md-6 col-sm-6 col-6">
+                                                        <div className=" px-2">
+                                                            {generateStars(item.stars)} ({item.votes} Votes)
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-md-6 col-sm-6 col-6">
+                                                        <div className="d-flex justify-content-end px-2">
+                                                            ({item.sold} Sold)
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div className="row">
+                                                    <div className="col-12">
+                                                        <div className=" px-2">
+                                                            <hr />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                
+                                                <div className="row">
+                                                    <div className="col-md-6 col-sm-6 col-6">
+                                                        <div style={{ color: '#696984' }} className=" px-2">
+                                                            Price : <span className="fs-5" style={{ color: "#0B7077" }}> ${item.price}</span>
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-md-6 col-sm-6 col-6">
+                                                        <div className="d-flex justify-content-end ">
+                                                            {item.column2}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+
+                        </div>
+                    </div>
+                </div>
+
+                {/* Pagination at the Bottom */}
+                <div className="py-1 ">
+                    <div className='container'>
+                        <div className='row'>
+                            <div className='d-flex col text-center justify-content-center align-items-center'>
+                                <Pagination>
+                                    <Pagination.Prev />
+                                    <Pagination.Item active>{1}</Pagination.Item>
+                                    <Pagination.Item>{2}</Pagination.Item>
+                                    <Pagination.Ellipsis />
+                                    <Pagination.Item>{9}</Pagination.Item>
+                                    <Pagination.Item >{10}</Pagination.Item>
+                                    <Pagination.Next />
+                                </Pagination>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default BrowseDesigns;
---
+++ Inspire Interiors/src/pages/Customer/Designs/designs.jsx
@@ -0,0 +1,190 @@
+import React, { useState } from 'react';
+import { Container, Row, Col, Card, Carousel } from 'react-bootstrap';
+import * as Icon from 'react-bootstrap-icons';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './../../../styles/customer/designs.css';
+import SearchPage from '../../../components/customer/filterNsearch';
+
+
+const Designs = () => {
+
+  const cards = [
+    {
+      title: 'Living Room', content: ['https://hips.hearstapps.com/hmg-prod/images/interior-design-ideas-idea-9-dwell-1-1644331792.jpg',
+        'https://havenly.com/blog/wp-content/uploads/2022/07/render-final-2232106-38554-8220-106864de852c.jpeg',
+        'https://hips.hearstapps.com/hmg-prod/images/interior-design-ideas-idea-9-dwell-1-1644331792.jpg',
+      ]
+    },
+    {
+      title: 'Bed Room', content: ['https://hips.hearstapps.com/hmg-prod/images/interior-design-ideas-idea-9-dwell-1-1644331792.jpg',
+        'https://hips.hearstapps.com/hmg-prod/images/interior-design-ideas-idea-9-dwell-1-1644331792.jpg'
+      ]
+    },
+    {
+      title: 'Dining Room', content: ['https://hips.hearstapps.com/hmg-prod/images/interior-design-ideas-idea-9-dwell-1-1644331792.jpg',
+        'https://hips.hearstapps.com/hmg-prod/images/interior-design-ideas-idea-9-dwell-1-1644331792.jpg'
+      ]
+    },
+    {
+      title: 'Office', content: ['https://hips.hearstapps.com/hmg-prod/images/interior-design-ideas-idea-9-dwell-1-1644331792.jpg',
+        'https://hips.hearstapps.com/hmg-prod/images/interior-design-ideas-idea-9-dwell-1-1644331792.jpg'
+      ]
+    },
+  ];
+
+  const designerCards = [
+    // Cards data for the first section...
+    {
+      name: 'Office',
+      image: 'https://hips.hearstapps.com/hmg-prod/images/interior-design-ideas-idea-9-dwell-1-1644331792.jpg',
+      votings: 10,
+      rate: 4.5,
+    },
+    {
+      name: 'Office',
+      image: 'https://hips.hearstapps.com/hmg-prod/images/interior-design-ideas-idea-9-dwell-1-1644331792.jpg',
+      votings: 70,
+      rate: 3.7,
+    },
+    {
+      name: 'Office',
+      image: 'https://hips.hearstapps.com/hmg-prod/images/interior-design-ideas-idea-9-dwell-1-1644331792.jpg',
+      votings: 10,
+      rate: 4.5,
+    },
+    {
+      name: 'Office',
+      image: 'https://hips.hearstapps.com/hmg-prod/images/interior-design-ideas-idea-9-dwell-1-1644331792.jpg',
+      votings: 10,
+      rate: 4.5,
+    },
+    {
+      name: 'Office',
+      image: 'https://hips.hearstapps.com/hmg-prod/images/interior-design-ideas-idea-9-dwell-1-1644331792.jpg',
+      votings: 18,
+      rate: 5,
+    },
+    {
+      name: 'Office',
+      image: 'https://hips.hearstapps.com/hmg-prod/images/interior-design-ideas-idea-9-dwell-1-1644331792.jpg',
+      votings: 10,
+      rate: 4.5,
+    },
+  ];
+
+  const generateStars = (rate) => {
+    const fullStars = Math.floor(rate);
+    const halfStar = rate - fullStars >= 0.5;
+
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      if (i <= fullStars) {
+        stars.push(<Icon.StarFill key={i} color='#f39c12' />);
+      } else if (i === fullStars + 1 && halfStar) {
+        stars.push(<Icon.StarHalf key={i} color='#f39c12' />);
+      } else {
+        stars.push(<Icon.Star key={i} color='#f39c12' />);
+      }
+    }
+
+    return stars;
+  };
+
+  return (
+    <>
+
+      <div className=' background flex-col'>
+        <div style={{ margin: "0px", padding: "0px" }} className='bg-light flex-col  w-98 rounded-3 mb-4 p0' >
+
+          <div className='top-bar flex-lg-2 flex-md-2 w-100 justify-content-between'>
+            {/* Top designers and see all link */}
+            <div className='row w-100 d-flex align-self-center '>
+              <div className='d-flex w-25 h-100 justify-content-center align-self-center'>
+                <h4>Top designers </h4>
+              </div>
+              <div className='d-flex col w-auto h-100 justify-content-start align-self-center' style={{ color: '#035C94' }}>
+                <h6>See All
+                  <Icon.ArrowRight
+                    color="#035C94"
+                    size={15}
+                    className="align-center"
+                  />
+                </h6>
+              </div>
+              <div className='col  d-flex justify-content-end'>
+                <SearchPage />
+
+              </div>
+
+            </div>
+
+          </div>
+
+          {/* Designers images */}
+          <div style={{ margin: "0px", padding: "0px" }} x className='bg-light image-bar row w-100 flex-row '>
+            {designerCards.map((card, index) => (
+              <div key={index} className='col-5 col-md-4 col-lg-2 mb-3'>
+                <Card className='h-100 w-100 rounded border-0 rounded' style={{ color: '#7C828B' }}>
+                  <Card.Img variant='top' src={card.image} className='rounded-3' />
+                  <Card.Body className='flex-row justify-content-center'>
+                    <Card.Text className='d-flex m-0 lead justify-content-center' >
+                      {card.name}
+                    </Card.Text>
+                    <Card.Text className='d-flex m-0 justify-content-center'>
+                      {generateStars(card.rate)}
+                    </Card.Text>
+                    <Card.Text className='d-flex m-0 justify-content-center '>
+                      {card.votings} Votes
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/*Explore by category */}
+        <div className='bg-light flex-col w-98 h-50 rounded-3' >
+          <div className='top-bar flex w-100 h-20'>
+            <div className='d-flex row align-self-center mt-2'>
+              <div className='d-flex row h-100 justify-content-center align-self-center'>
+                <h4>Explore by Rooms</h4>
+              </div>
+              <div className='d-flex row h-100 justify-content-start align-self-center mt-1' style={{ color: '#035C94' }}>
+                <h6>Room Specialists</h6>
+              </div>
+            </div>
+          </div>
+          {/* Images */}
+          <div className='d-flex flex-column flex-lg-row flex-md-row flex-sm-row w-100 justify-content-center align-self-center'>
+            {cards.map((card, index) => (
+              <Col md={3} key={index} className='d-flex'>
+                <Card className='d-flex h-100 w-100 rounded border-0' >
+                  <Card.Body className='d-flex'>
+                    <Carousel >
+                      {card.content.map((imageUrl, imageIndex) => (
+                        <Carousel.Item key={imageIndex}>
+                          {/* {card.title} */}
+                          <img
+                            className='d-flex w-100 rounded-3 img-fluid h-100'
+                            src={imageUrl}
+                            alt={`Slide ${imageIndex + 1}`}
+                          />
+                        </Carousel.Item>
+                      ))}
+                    </Carousel>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </div>
+
+
+        </div>
+      </div>
+
+    </>
+  );
+};
+
+export default Designs;
