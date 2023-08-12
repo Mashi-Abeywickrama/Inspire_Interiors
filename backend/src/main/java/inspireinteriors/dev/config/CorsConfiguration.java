c6fd1881c68e1a9ebdef2dbc93f1484c7157086f
Shinthujen-I <shinthujeni@gmail.com>
2023-08-12 12:28:30 +0530

    Cors configuration update

--- backend/src/main/java/inspireinteriors/dev/config/CorsConfiguration.java
+++ backend/src/main/java/inspireinteriors/dev/config/CorsConfiguration.java
@@ -13,7 +13,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // Add your React frontend URL here
+                        .allowedOrigins("http://127.0.0.1:5173","http://localhost:5173/") // Use the correct origin URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
 
