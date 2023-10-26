1997e277d26679665f131c080d273033302b1546
Shinthujen-I <shinthujeni@gmail.com>
2023-10-26 11:24:48 +0530

    Cart Initialized

---
+++ backend/src/main/java/inspireinteriors/dev/repository/CartRepository.java
@@ -0,0 +1,11 @@
+package inspireinteriors.dev.repository;
+
+import inspireinteriors.dev.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart,Integer> {
+
+}
+
